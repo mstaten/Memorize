8ae6d10d71fddffae539bb0e2a0cc99de62a3e50
Michelle <michelle@thinkhappymedium.com>
2023-09-08 16:44:04 -0500

    Remove comments, begin assignment 1 tasks #1 - 8

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,51 +7,142 @@
 
 import SwiftUI
 
-// Lesson 1: "behaves like a..." vs "is a..."
-/// this is functional programming. The focus is on the functionality, the behavior, and not on the data.
-/// OOP - its roots are data encapsulation.
-/// Functional prog. is more like behavior encapsulation, but much more. it's all about behavior - how do things behave, how do structs behave...
-// "This struct, ContentView, behaves like a View"
-// difference is that View is not a struct, it's a protocol - a thing you can behave like
-struct ContentView: View {
+enum ThemeType: String, CaseIterable {
+    case halloween, flowers, green
     
-    // "This variable, i, is of type Int"
-    
-    // normal properties, stored properties (not really a proper name)
-//    var i: Int
-//    var s: String
-    
-    // computed property
-    
-    // computed: the value of this variale is not stored somewhere; it's computed.
-    // every time it's asked for, it's computed. It's running a lot.
-    // it's like a read-only variable because you can only compute the value
-    
-    // the type of this variable can be any struct in the world as long as it behaves like a View
-    var body: some View {
-        HStack {
-            CardView()
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
+    var emojis: [String] {
+        switch self {
+        case .halloween:
+            return ["👻", "👻", "🎃", "🎃", "💀", "💀", "😈", "😈", "🕷️", "🕷️", "🕸️", "🕸️",
+                    "🍫", "🍫", "🍬", "🍬", "🧟", "🧟", "🧙‍♀️", "🧙‍♀️", "👹", "👹", "☠️", "☠️"]
+        case .flowers:
+            return ["🌼", "🌼", "🌸", "🌸", "💐", "💐", "🌹", "🌹", "🌻", "🌻", "🪻", "🪻",
+                    "🌺", "🌺", "🪷", "🪷", "🌷", "🌷", "🥀", "🥀"]
+        case .green:
+            return ["💚", "💚", "🥝", "🥝", "🐍", "🐍", "🐸", "🐸", "🦠", "🦠", "🌱", "🌱",
+                    "🌴", "🌴", "🦖", "🦖", "🍀", "🍀", "📗", "📗", "🍐", "🍐"]
         }
-        .foregroundColor(.orange)
+    }
+    
+    var cardColor: Color {
+        switch self {
+        case .halloween: return .orange
+        case .flowers: return .pink
+        case .green: return .green
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .halloween: return "Halloween"
+        case .flowers: return "Flowers"
+        case .green: return "Green"
+        }
+    }
+    
+    var groupImage: String {
+        switch self {
+        case .halloween: return "🎃"
+        case .flowers: return "🌸"
+        case .green: return "💚"
+        }
+    }
+}
+
+struct ContentView: View {
+    @State var theme: ThemeType = .green
+    @State var emojis = ThemeType.green.emojis.shuffled()
+    @State var cardCount: Int = 4
+    
+    var themes: [ThemeType] = ThemeType.allCases
+    
+    var body: some View {
+        VStack {
+            Text("Memorize!").font(.largeTitle).fontWeight(.bold)
+            themePicker
+            ScrollView {
+                cards
+            }
+            cardCountAdjusters
+        }
         .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) {index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(theme.cardColor)
+    }
+    
+    var themePicker: some View {
+        HStack {
+            ForEach(themes.indices, id: \.self) { index in
+                Button(action: {
+                    theme = themes[index]
+                    emojis = theme.emojis.shuffled()
+                    if cardCount > emojis.count {
+                        cardCount = emojis.count
+                    }
+                }, label: {
+                    VStack {
+                        Text(themes[index].groupImage)
+                        Text(themes[index].name).font(.headline)
+                    }
+                })
+            }
+        }
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > theme.emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "minus.circle")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "plus.circle")
     }
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    let content: String
+    @State var isFaceUp: Bool = true
     
     var body: some View {
-        if isFaceUp {
-            ZStack {
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+        let base: RoundedRectangle = .init(cornerRadius: 12)
+        ZStack {
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
             }
-        } else {
-            RoundedRectangle(cornerRadius: 12)
+            .opacity(isFaceUp ? 1 : 0)
+            
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
